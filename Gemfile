d49b2299934fac64ac3439fd611a170664eeff05
ssw <ssw@ssw.com>
2018-04-18 12:07:47 +0800

    使用了访问远程服务器的办法,来验证 devise

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'http://gems.ruby-china.org'
 gemspec
 
 gem 'mysql2', '~> 0.3.11'
-
+gem 'devise'
+gem 'httparty'
--- lib/casserver/authenticators/rails_devise.rb
+++ lib/casserver/authenticators/rails_devise.rb
@@ -1,19 +1,21 @@
 require 'rubygems'
 require 'casserver/authenticators/base'
 require 'active_record'
+require 'httparty'
 
 
 # 定制的Rails Devise 验证
+# 直接访问远程
 module CASServer
   module Authenticators
     class RailsDevise < CASServer::Authenticators::Base
       def validate(credentials)
         read_standard_credentials(credentials)
-        return true if true
-        return false if @password.blank?
-        @user = User.find_by_email(@username)
-        return false if @user.blank?
-				return @user.valid_password? @password
+        return false if @password.blank? || @username.blank?
+        url = "http://coiex.io/internal_interfaces/is_password_valid?email=#{@username}&password=#{@password}"
+        result = HTTParty.get(url).body.to_s == 'true' rescue false
+        #logger.info "====== result: #{result}"
+        return result
       end
     end
   end
